83b4554308fcf313eb084a5b8335d8a6692b697f
Andrew Zschorn <andrew.zschorn@precisionpc.com.au>
2015-10-14 21:30:50 +1030

    tried a service from a plugin :(

--- grails-app/services/test/unit/service/mock/AuthorService.groovy
+++ grails-app/services/test/unit/service/mock/AuthorService.groovy
@@ -1,11 +1,14 @@
 package test.unit.service.mock
 
 import grails.transaction.Transactional
+import grails.plugin.segmentio.SegmentioService
 
 @Transactional
 class AuthorService {
 
     BookService bookService
+
+    SegmentioService segmentioService
 
     def anAuthorship() {
         bookService.aBookishThing()
--- src/test/groovy/test/unit/service/mock/AuthorServiceSpec.groovy
+++ src/test/groovy/test/unit/service/mock/AuthorServiceSpec.groovy
@@ -3,6 +3,7 @@ package test.unit.service.mock
 import grails.test.mixin.TestFor
 import grails.test.mixin.Mock
 import spock.lang.Specification
+import grails.plugin.segmentio.SegmentioService
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
@@ -19,8 +20,9 @@ class AuthorServiceSpec extends Specification {
 
     void "test something"() {
         given:
-        BookService mockBookService = Mock(BookService)
-        service.bookService = mockBookService
+        //        BookService mockBookService = Mock(BookService)
+        //        service.bookService = mockBookService
+        SegmentioService mockSegmentioService = Mock(SegmentioService)
         
         expect:"fix me"
             true 
